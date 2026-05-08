4d78ff2b4e339924837db52dbda9e233ba0eae91
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-08 13:24:40 +0300

    Extract GradientContainer to a separate file

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lab3_app/gradient_container.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white, Colors.blue, Colors.red],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          "Hello world!",
+          style: TextStyle(color: Colors.white, fontSize: 32),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: (Scaffold
-    (body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.white,
-            Colors.blue,
-            Colors.red,
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Center(
-        child: Text(
-          "Hello world",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
-          ),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: (Scaffold(body: GradientContainer())),
     ),
-      )),
-      ),
-      );
+  );
 }
+
+
