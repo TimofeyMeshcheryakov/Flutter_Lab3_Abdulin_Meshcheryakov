7c3e6293354addfa09b211a7da7f3f3b50c0f197
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-08 13:58:21 +0300

    Style roll button and adjust layout

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -31,12 +31,22 @@ void rollDice() {}
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/dice-1.png',
             width: 300,
             ),
             TextButton(
-              onPressed: rollDice, 
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                foregroundColor: Colors.lime,
+                textStyle: const TextStyle(
+                  fontSize: 30,
+                ),
+              ), 
               child: Text("Roll Dice"),
               ),
           ],
