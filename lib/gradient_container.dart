30e569f9f475d926659f56cf9018b942f0c91e3a
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-08 14:30:05 +0300

    Add random dice roll logic

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab3_app/dice_roller.dart';
-import 'package:flutter_lab3_app/styled_text.dart';
+
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
